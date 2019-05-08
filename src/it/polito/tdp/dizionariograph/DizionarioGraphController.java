f6cefeee100cd9535d87f33a458f720052947fb4
Andrea <Andrea@172.22.15.88>
2019-05-08 15:17:16 +0200

    Risolto

--- src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
+++ src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
@@ -1,5 +1,106 @@
 package it.polito.tdp.dizionariograph;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.dizionariograph.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class DizionarioGraphController {
 	
+	private Model modello = new Model();
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtNumero;
+
+    @FXML
+    private TextField txtParola;
+
+    @FXML
+    private Button btnGrafo;
+
+    @FXML
+    private Button btnVicini;
+
+    @FXML
+    private Button btnGradoMax;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    void doGradoMax(ActionEvent event) {
+
+    	txtResult.clear();
+    	txtResult.appendText("Parola: "+modello.findMaxDegree()+",grado: "+modello.displayNeighbours(modello.findMaxDegree()).size()+"\n");
+    	for(String s : modello.displayNeighbours(modello.findMaxDegree()))
+    		txtResult.appendText(s+"\n");
+    	
+    }
+
+    @FXML
+    void doGrafo(ActionEvent event) {
+    	
+    	try {
+    	modello.createGraph(Integer.parseInt(this.txtNumero.getText()));
+    	this.btnVicini.setDisable(false);
+    	this.txtParola.setDisable(false);
+    	} catch (NumberFormatException e) {
+    		e.printStackTrace();
+    		txtResult.appendText("inserisci un numero");
+    	}
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+    	
+    	txtResult.clear();
+    	this.txtNumero.clear();
+    	this.txtParola.clear();
+    	
+    }
+
+    @FXML
+    void doVicini(ActionEvent event) {
+
+    	txtResult.clear();
+    	modello.displayNeighbours(this.txtParola.getText());
+    	this.btnGradoMax.setDisable(false);
+    	txtResult.appendText("Parole simili : \n");
+    	for(String s : modello.displayNeighbours(this.txtParola.getText()))
+    		txtResult.appendText(s+"\n");
+    }
+    
+    public void setModel(Model m) {
+    	this.modello = m;
+    	this.btnGradoMax.setDisable(true);
+    	this.btnVicini.setDisable(true);
+    	this.txtParola.setDisable(true);
+    }
+
+    @FXML
+    void initialize() {
+        assert txtNumero != null : "fx:id=\"txtNumero\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnGrafo != null : "fx:id=\"btnGrafo\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnVicini != null : "fx:id=\"btnVicini\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnGradoMax != null : "fx:id=\"btnGradoMax\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+
+    }
 }
--- src/it/polito/tdp/dizionariograph/model/Model.java
+++ src/it/polito/tdp/dizionariograph/model/Model.java
@@ -3,21 +3,86 @@ package it.polito.tdp.dizionariograph.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+import it.polito.tdp.dizionariograph.db.WordDAO;
+
 public class Model {
-
+	
+	WordDAO dao = new  WordDAO();
+	List<String> parole = new ArrayList<String>();
+	Graph<String, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
+	
 	public void createGraph(int numeroLettere) {
+		
+		parole = dao.getAllWordsFixedLength(numeroLettere);
+		
+		Graphs.addAllVertices(g, parole);
+		
+		for(String s : parole) {
+			
+			for(String s2 : parole) {
+				
+				if(!s.equals(s2)) {
+					if(controlla(s,s2))
+						g.addEdge(s, s2);
+				}
+			}
+		}
+		
+		System.out.println(g);
 
-		System.err.println("createGraph -- TODO");
+	}
+
+	private boolean controlla(String s, String s2) {
+		// TODO Auto-generated method stub
+		int conto = 0;
+		
+		for(int i = 0 ; i < s.length() ; i++) {
+			
+			if(s.charAt(i) != s2.charAt(i))
+				conto++;
+			
+			if(conto > 1)
+				return false;
+		}
+		
+		return true;
 	}
 
 	public List<String> displayNeighbours(String parolaInserita) {
 
-		System.err.println("displayNeighbours -- TODO");
-		return new ArrayList<String>();
+		List<String> vicini = new ArrayList<String>();
+		for(String s : this.parole) {
+			
+			if(g.containsEdge(parolaInserita, s))
+				vicini.add(s);
+			
+		}
+		
+		if(vicini.size() == 0) {
+			vicini.add("nessuna parola");
+		}
+		return vicini;
 	}
 
-	public int findMaxDegree() {
-		System.err.println("findMaxDegree -- TODO");
-		return -1;
+	public String findMaxDegree() {
+		
+		int max = 0;
+		String migliore = "";
+		
+		for(String s : g.vertexSet()) {
+			 
+			if(g.degreeOf(s) > max ) {
+				max = g.degreeOf(s);
+				migliore = s;
+			}
+		}
+		
+		return migliore;
+			
 	}
 }
